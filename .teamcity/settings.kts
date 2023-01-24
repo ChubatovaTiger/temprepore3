e54963526d359d8f8a900ac1fd7623620a8e9c4b
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2023-01-24 08:59:32 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -26,12 +26,26 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2022.10"
 
 project {
-
-    subProject(Proj1)
+    for (i in 0..3) {
+        subProject {
+            id("Proj$i")
+            name = "Proj$i"
+            rootid="Proj$i_Root$i"
+            vcsRoot(rootid)
+                buildType {
+                    id("Build$i")
+                    name = "Build$i"
+                    
+                    vcs {
+                        
+                        }
+            }
+        }
+    }
 }
 
 
-object Proj1 : Project({
+/*object Proj1 : Project({
     name = "proj1"
 
     vcsRoot(Proj1_Root1)
@@ -46,9 +60,24 @@ object Proj1_Build1 : BuildType({
         root(Proj1_Root1)
     }
 })
+*/
 
 object Proj1_Root1 : GitVcsRoot({
     name = "root1"
+    pollInterval = 6
+    url = "https://github.com/ChubatovaTiger/manyBranches"
+    branch = "refs/heads/12e"
+    branchSpec = "refs/heads/(*)"
+})
+object Proj2_Root2 : GitVcsRoot({
+    name = "root2"
+    pollInterval = 6
+    url = "https://github.com/ChubatovaTiger/manyBranches"
+    branch = "refs/heads/12e"
+    branchSpec = "refs/heads/(*)"
+})
+object Proj3_Root3 : GitVcsRoot({
+    name = "root3"
     pollInterval = 6
     url = "https://github.com/ChubatovaTiger/manyBranches"
     branch = "refs/heads/12e"
